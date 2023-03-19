0d635366b7e2dfc97f021b97290120174278ae98
njeribena <njeriwaweru94@gmail.com>
2023-03-19 06:44:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar("\n");
+		return (0);
+}
